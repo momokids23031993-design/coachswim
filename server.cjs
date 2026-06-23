cab9361a17dc7de3564f12a60c4d6b2e417d741d
momokids23031993-design <momokids23031993@gmail.com>
2026-06-23 20:38:04 +0700

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -1387,6 +1387,8 @@ ORDER BY j.start ASC
 app.use("/foto", express.static(path.join(__dirname, "foto")));
 
 // ================== RUN SERVER ==================
-app.listen(5000, "localhost", () => {
-  console.log("Server SQLite jalan di localhost:5000 🚀");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server jalan di port ${PORT}`);
 });
